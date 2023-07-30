161ca4e253a4cff7bc1bc6fbcd04209247fa6323
thugperfect <meenamani9444@gmail.com>
2023-07-30 09:22:19 +0000

    added footer..

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Hola from './components/Hola';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
     <Hola/>
-   
+    
+   <Footer/>
     </div>
   );
 }
--- src/components/Hola.js
+++ src/components/Hola.js
@@ -4,6 +4,8 @@ function Hola() {
         <h1 className='myname'>Subramanian M</h1>
         <h3 className='edu'>Final Year Computer Science Engineering</h3>
         <h4 className='objective'>A Highly motivated and fully oriented Full-Stack Developer</h4>
+
+        <div id="contact-me">Contact Me</div>
     </div>
   </div>)
 }
