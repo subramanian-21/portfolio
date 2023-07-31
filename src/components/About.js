ff13a7638ef883cc7139aba32f7ca662d9bffc97
thugperfect <meenamani9444@gmail.com>
2023-07-31 13:35:46 +0000

    about complete...

--- src/components/About.js
+++ src/components/About.js
@@ -10,19 +10,55 @@ function About() {
             Iam a MERN stack developer . I am interested in Full Stack positions, here you can see some of my projects, if you like what you see do not hesitate to contact me.
 
         </div>
-        <div id="contact-me">Contact Me</div>
+        <div id="contact-me">Collaborate</div>
         </div>
        
         <div className="tech-stack">
             <div className="all-tech-stack">
                 <div className="top-stack">
-                     <div className="top-left" style={{ backgroundImage: `url(${background1})` }}></div>
-                     <div className="top-right" style={{ backgroundImage: `url(${background2})` }}></div>
+                     <div className="top-left" style={{ backgroundImage: `url(${background1})` }}>
+                        <div className='about-heading'>Languages</div>
+                        <ul className='bold '>
+                        <li>C</li>
+                        <li>Java</li>
+                        <li>JavaScript</li>
+                        
+                        </ul>
+                     </div>
+                     <div className="top-right" style={{ backgroundImage: `url(${background2})` }}>
+
+                     <div className='about-heading black'>Frameworks and libraries</div>
+                     <ul className='black bold'>
+                     <li>Node</li>
+                        <li>React</li>
+                        <li>Express</li>
+                        <li>Axios</li>
+                        <li>Mongoose</li>
+                       
+                        </ul>
+                     </div>
                 </div>
                 <div className="bottom-stack">
                         
-                    <div className="bottom-left" style={{ backgroundImage: `url(${background3})` }}></div>
-                    <div className="bottom-right" style={{ backgroundImage: `url(${background4})` }}></div>
+                    <div className="bottom-left" style={{ backgroundImage: `url(${background3})` }}>
+                    <div className='about-heading'>Skills</div>
+                    <ul className='bold'>
+                        <li>VS Code</li>
+                        <li>Git & Github</li>
+                        <li>Terminal</li>
+                        <li>Postman</li>
+                      
+                        </ul>
+                    </div>
+                    <div className="bottom-right" style={{ backgroundImage: `url(${background4})` }}>
+                    <div className='about-heading black'>DataBase</div>
+                    <ul className='black bold' >
+                        <li>SQL</li>
+                        <li>MongoDB</li>
+                       
+                      
+                        </ul>
+                    </div>
                 </div>
             </div>
         
--- src/components/Hola.js
+++ src/components/Hola.js
@@ -16,7 +16,7 @@ setHover(!hover)
         <h3 className='edu'>Final Year Computer Science Engineering</h3>
         <h4 className='objective'>A Highly motivated and fully oriented Full-Stack Developer</h4>
 
-        <div id="contact-me">Contact Me</div>
+        <div id="contact-me">Collaborate</div>
         </div>
        
 
