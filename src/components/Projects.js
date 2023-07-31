e798fa0f56fd4dd51d91fb81ffa235d97f3e4fef
thugperfect <meenamani9444@gmail.com>
2023-07-31 11:38:03 +0000

    projects complete...

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,6 +9,7 @@ import imagetoolkit from '../images/imagetoolkit.png'
 import imagetoolkitport from '../images/imagetoolkitportrait.png'
 import mathmagician from '../images/mathmagician.png'
 import mathmagicianport from '../images/mathmagicianportrait.png'
+import newspro from '../images/newspro.png'
 
 
 function Projects(params) {
@@ -24,28 +25,39 @@ function Projects(params) {
                         <div className="q1">
                             <div className="o1">
                                 <div className="n1"  > <img className='img' src={hangman} alt=""/>
-                                <div className="hangman"></div>
+                                <div className="hangman">
+                                    <div className='pro-main'> <div className='title'>Hangman Game</div>
+                                    <div className='sub'>Hangman game using vanilla Js</div></div>
+                                   
                                 </div>
-                                <div className="n2"><img className='img' src={handcricket} alt=""/>
+                                </div>
+                                <div className="n2"><img className='img' src={mathmagician} alt=""/>
                                 
-                                <div className='handcricket'></div></div>
+                                <div className='mathmagician'>
+                                <div className='pro-main'> <div className='title'>Math Magician</div>
+                                    <div className='sub'>It is just a riddle app with calculator which fetches questions from json</div></div></div></div>
                             </div>
                             <div className="o2"><img className='img' src={imagefetch} alt=""/>
                             
-                            <div className='imagefetch'></div></div>
+                            <div className='imagefetch'>
+                            <div className='pro-main'> <div className='title'>Hangman Game</div>
+                                    <div className='sub'>Hangman game using vanilla Js</div></div></div></div>
                         </div>
                         <div className="q2"><img className='img' src={imagetoolkitport} alt=""/>
                         
-                        <div className='imagetoolkit'></div></div>
+                        <div className='imagetoolkit'><div className='pro-main'> <div className='title'>Hangman Game</div>
+                                    <div className='sub'>Hangman game using vanilla Js</div></div></div></div>
                     </div>
                     <div className="p2">
-                        <img className='img' src={chatthru} alt=""/>
-                        <div className='chatthru'></div>
+                        <img className='img' src={newspro} alt=""/>
+                        <div className='newspro'><div className='pro-main'> <div className='title'>Hangman Game</div>
+                                    <div className='sub'>Hangman game using vanilla Js</div></div></div>
                     </div>
                 </div>
                 <div className="m2">
-                    <img className="img" src={mathmagicianport} alt="none"/>
-                    <div className='mathmagician'></div>
+                    <img className="img" src={chatthruport} alt="none"/>
+                    <div className='chatthru'><div className='pro-main'> <div className='title'>Hangman Game</div>
+                                    <div className='sub'>Hangman game using vanilla Js</div></div></div>
                 </div>
                 </div>
                 </div>
