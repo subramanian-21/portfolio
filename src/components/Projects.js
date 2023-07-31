b33e94fa26ea4a9ab31a42b9a7b26653c5b1edf6
thugperfect <meenamani9444@gmail.com>
2023-07-31 07:51:25 +0000

    imported all projects...

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,3 +1,15 @@
+import chatthruport from '../images/chatthrupotrait.png'
+import chatthru from '../images/chatthrublack.png'
+import imagefetch from '../images/imagefinder.png'
+import imagefetchport from '../images/imagefinderportrait.png'
+import hangman from '../images/hangman.png'
+import hangmanport from '../images/hangmanportrait.png'
+import handcricket from '../images/handcricket.png'
+import imagetoolkit from '../images/imagetoolkit.png'
+import imagetoolkitport from '../images/imagetoolkitportrait.png'
+import mathmagician from '../images/mathmagician.png'
+import mathmagicianport from '../images/mathmagicianportrait.png'
+
 function Projects(params) {
     return(
         <div className="p-body">
@@ -8,16 +20,20 @@ function Projects(params) {
                     <div className="p1">
                         <div className="q1">
                             <div className="o1">
-                                <div className="n1"></div>
+                                <div className="n1"> <img className='img' src={chatthru} alt=""/></div>
                                 <div className="n2"></div>
                             </div>
                             <div className="o2"></div>
                         </div>
                         <div className="q2"></div>
                     </div>
-                    <div className="p2"></div>
+                    <div className="p2">
+                        <img className='img' src={chatthru} alt=""/>
+                    </div>
                 </div>
-                <div className="m2"></div>
+                <div className="m2">
+                    <img className="img" src={mathmagicianport} alt="none"/>
+                </div>
                 </div>
             </div>
         </div>
