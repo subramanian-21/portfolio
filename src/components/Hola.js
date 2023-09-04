5bab2243613c6164cc07e0d0ae3091e5a27b3ea4
thugperfect <meenamani9444@gmail.com>
2023-09-04 08:02:14 +0530

    added bored?

--- src/components/Hola.js
+++ src/components/Hola.js
@@ -9,8 +9,9 @@ setHover(!hover)
   }
   console.log(hover);
   return( <div ref={ref} className='body'>
-  <div className='innerbody color-change bg-slate-700 outline outline-1 outline-gray-600'>
-
+  <div className='innerbody color-change bg-black outline outline-1 outline-gray-600'>
+  <div className="w-4/5 mt-4  text-xl text-white mr-5 flex justify-end ">
+    <div className="bg-[#282c34] pt-2 pb-2 pl-5 pr-5 hover:bg-slate-100 hover:text-black rounded-full">bored?</div></div>
       <div className="minnerbody">
       <div className='myname  text-white px-3'>Subramanian M</div>
       <h3 className='edu px-3 text-gray-400'>Final Year Computer Science Engineering</h3>
