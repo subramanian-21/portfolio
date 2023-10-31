6af903a7d2b7faed87d25c400d41b041f0f938e5
thugperfect <meenamani9444@gmail.com>
2023-10-31 19:06:31 +0530

    complete

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -57,6 +57,58 @@ return(
 
 </div>
            
+
+
+<div className="m3">
+            <div className="m1">
+                <div className="p1">
+                    <div className="q1">
+                        <div className="o1">
+                            <a rel='noreferrer' target="_blank" href='https://github.com/thugperfect/hangman-game' className="n1"  > <img className='img' src={hangman} alt=""/>
+                            <div className="hangman">
+                                <div className='pro-main'> <div className='title'>Hangman Game</div>
+                                <div className='sub'>Hangman game using vanilla Js</div>
+                                <div className='stack'>Tech Stack :HTML,CSS,JS</div>
+                                </div>
+                               
+                            </div>
+                            </a>
+                            <a rel='noreferrer' target="_blank" href='https://github.com/thugperfect/math-magicians' className="n2"><img className='img' src={mathmagician} alt=""/>
+                            
+                            <div className='mathmagician'>
+                            <div className='pro-main'> <div className='title'>Math Magician</div>
+                                <div className='sub'>It is just a riddle app with calculator which fetches questions from json</div>
+                                <div className='stack'>Tech Stack :React, Sass, JSON</div></div></div></a>
+                        </div>
+                        <a rel='noreferrer' target="_blank" href='https://github.com/thugperfect/ImageSearcher' className="o2"><img className='img' src={kumazon} alt=""/>
+                        
+                        <div className='imagefetch'>
+                        <div className='pro-main'> <div className='title'>Image Fetch</div>
+                                <div className='sub'>An Ecommerce App with product searching and Adding to Cart feature</div>
+                                <div className='stack'>Tech Stack :React, Redux-toolkit, React-Redux...</div></div></div></a>
+                    </div>
+                    <a rel='noreferrer' target="_blank" href='https://github.com/thugperfect/ImageToolKit' className="q2"><img className='img' src={imagetoolkitport} alt=""/>
+                    
+                    <div className='imagetoolkit'><div className='pro-main'> <div className='title'>ImageToolKit</div>
+                                <div className='sub'>ImageToolkit is a image altering application that does fetches the image and converts it into required at the backend using JIMP</div>
+                                <div className='stack'>Tech Stack :Angular, Express, Jimp, Cors, ...</div></div></div></a>
+                </div>
+                <a rel='noreferrer' target="_blank" href='https://github.com/thugperfect/playfolio' className="p2">
+                    <img className='img' src={playfolio} alt=""/>
+                    <div className='newspro'><div className='pro-main'> <div className='title'>PlayFolio</div>
+                                <div className='sub'>Playfolio contains all games fetched from RAWG Api</div>
+                                <div className='stack'>Tech Stack :Angular, Api</div>
+                                </div></div>
+                </a>
+            </div>
+            <a rel='noreferrer' target="_blank" href='https://github.com/thugperfect/chatThru' className="m2">
+                <img className="img" src={chatthruport} alt="none"/>
+                <div className='chatthru'><div className='pro-main'> <div className='title'>ChatThru</div>
+                                <div className='sub '>A Social Media Application that provides users with CRUD operations..</div>
+                                
+                                <div className='stack'>Tech Stack : React, Express, Mongoose, MongoDB, ..</div></div></div>
+            </a>
+            </div>
         
             <hr className='h-[5px] bg-white rounded-xl'/>
             </div>
